7d241366400c7961d23664c97a708f7cf039d637
Peter Vos <pjmvos@gmail.com>
2024-10-23 16:25:07 +0200

    formatting

--- irodsdata.py
+++ irodsdata.py
@@ -5,48 +5,52 @@ from setup_session import setup_session
 from logger import logger
 import re
 
+
 def handle_exception():
     raise SystemExit(0)
 
 
-class IrodsData():
+class IrodsData:
     def __init__(self):
         self.session = None
-        self.data = {'collections': {}, 'groups': []}
-        self.logger=logger
+        self.data = {"collections": {}, "groups": []}
+        self.logger = logger
 
     def collect(self):
-        self.data['collections'] = self.get_home_collections()
-        self.data['groups'] = self.get_groups()
+        self.data["collections"] = self.get_home_collections()
+        self.data["groups"] = self.get_groups()
 
         group_members = []
-        for g in self.data['groups']:
-            group_members = list(set(group_members + self.data['groups'][g]['members']))
-            group_members = list(set(group_members + self.data['groups'][g]['read_members']))
+        for g in self.data["groups"]:
+            group_members = list(set(group_members + self.data["groups"][g]["members"]))
+            group_members = list(
+                set(group_members + self.data["groups"][g]["read_members"])
+            )
         return self.data
 
     def close_session(self):
         self.session.cleanup()
         self.session = None
-        self.logger.info('irods session closed')
+        self.logger.info("irods session closed")
 
     def get_session(self):
         try:
-            self.logger.info('setup irods session')
+            self.logger.info("setup irods session")
             self.session = setup_session()
             self.get_home_collections()  # try once to see if we are logged in
         except:
-            self.logger.error('could not get collections and groups, probably an authentication error')
+            self.logger.error(
+                "could not get collections and groups, probably an authentication error"
+            )
             handle_exception()
         return True
 
     def get_home_collections(self):
         collections = {}
-        coll = self.session.collections.get(f'/{self.session.zone}/home')
+        coll = self.session.collections.get(f"/{self.session.zone}/home")
         for col in coll.subcollections:
             collections[col.name] = {}
         return collections
-
 
     def get_member_count(self, group_name):
         internal = 0
@@ -60,22 +64,27 @@ class IrodsData():
 
     def get_groups(self):
         groups = {}
-        for path in self.data['collections']:
-            if path.startswith('research-') or path.startswith('datamanager-'):
+        for path in self.data["collections"]:
+            if path.startswith("research-") or path.startswith("datamanager-"):
                 groupname = path
                 groups[groupname] = {}
                 group_obj = self.session.user_groups.get(groupname)
-                groups[groupname]['category'] = group_obj.metadata.get_one('category').value
+                groups[groupname]["category"] = group_obj.metadata.get_one(
+                    "category"
+                ).value
                 try:
-                    groups[groupname]['data_classification'] = group_obj.metadata.get_one('data_classification').value
+                    groups[groupname]["data_classification"] = (
+                        group_obj.metadata.get_one("data_classification").value
+                    )
                 except:
-                    groups[groupname]['data_classification'] = "NA"
+                    groups[groupname]["data_classification"] = "NA"
                 member_names = [user.name for user in group_obj.members]
-                groups[groupname]['members'] = member_names
-                groups[groupname]['read_members'] = []
-                if path.startswith('research-'):
-                    read_group_obj = self.session.user_groups.get(groupname.replace('research-', 'read-', 1))
+                groups[groupname]["members"] = member_names
+                groups[groupname]["read_members"] = []
+                if path.startswith("research-"):
+                    read_group_obj = self.session.user_groups.get(
+                        groupname.replace("research-", "read-", 1)
+                    )
                     read_member_names = [user.name for user in read_group_obj.members]
-                    groups[groupname]['read_members'] = read_member_names
+                    groups[groupname]["read_members"] = read_member_names
         return groups
-
--- logger.py
+++ logger.py
@@ -1,13 +1,14 @@
 import logging
 from datetime import datetime
 
+
 def get_logger():
     LOGFILE = f"./log/datamamager-report_{datetime.now().strftime('%Y%m%d')}.log"
-    logger = logging.getLogger('yoda_datamanager_report')
+    logger = logging.getLogger("yoda_datamanager_report")
     logger.propagate = False
     logger.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
     hdlr = logging.FileHandler(LOGFILE)
     hdlr.setFormatter(formatter)
     ch = logging.StreamHandler()
@@ -17,5 +18,5 @@ def get_logger():
         logger.addHandler(ch)
     return logger
 
-logger = get_logger()
 
+logger = get_logger()
